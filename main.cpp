8916622cbb7daee2465ddddd872c6e0dc1d4cb7d
MauAguero-12 <mam.121002@gmail.com>
2021-03-17 19:50:48 -0600

    crear curvas

--- main.cpp
+++ main.cpp
@@ -3,59 +3,95 @@
 
 using namespace std;
 
-class punto{
+class arco{
 public:
-    double x;
-    double y;
+    int x;
+    int y;
+    int ancho;
+    int alto;
+    double inicio;
+    double final;
 
-    punto(double pX, double pY){
+    arco(int pX, int pY, int pAncho, int pAlto, double pInicio, double pFinal){
         x = pX;
         y = pY;
+        ancho = pAncho;
+        alto = pAlto;
+        inicio = pInicio;
+        final = pFinal;
     }
 };
-void crearImagen(int pAltura, int pAnchura, vector<punto> &pVector);
+
+void crearImagen(int pAltura, int pAnchura, vector<arco> &pVector);
 
 int main(){
-
-    //sepa diosito como vamos a hacer esto
-
-    vector<punto> vectorFigura;
+    vector<arco> vectorFigura;
     crearImagen(5, 5, vectorFigura);
 
     for(auto pun : vectorFigura){
-        cout << "(" << pun.x << ", " << pun.y << ")" << endl;
+        cout << "arc(" << pun.x << ", " << pun.y << ", " << pun.ancho << ", " << pun.alto << ", "
+        << pun.inicio << ", " << pun.final << ")" << endl;
     }
 
     return 0;
 }
 
-void crearImagen(int pAltura, int pAnchura, vector<punto> &pVector){
-    double y1 = 0.25;
-    double y2 = 0.25;
-    double y3 = 0.75;
-    double y4 = 0.75;
-
-    for (double x = 0; x <= 1; x = x + 0.01){
-        pVector.push_back(punto(x*pAnchura, y1*pAltura));
-        pVector.push_back(punto(x*pAnchura, y2*pAltura));
-        pVector.push_back(punto(x*pAnchura, y3*pAltura));
-        pVector.push_back(punto(x*pAnchura, y4*pAltura));
-
-        if (x < 0.25 || (x >= 0.5 && x < 0.75)){
-            y1 = y1 - 0.01;
-            y2 = y2 + 0.01;
-            y3 = y3 - 0.01;
-            y4 = y4 + 0.01;
-        } else{
-            y1 = y1 + 0.01;
-            y2 = y2 - 0.01;
-            y3 = y3 + 0.01;
-            y4 = y4 - 0.01;
-        }
-
-        if (y1 < 0.009){
-            y1 = 0;
-        }
+void crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
+    double pi = 3.1415927;
+    int size;
+    if (pAltura < pAnchura){
+        size = pAltura;
+    } else{
+        size = pAnchura;
     }
+
+    int pntx=10;
+    int pnty=80;
+    int ancho=1;
+    int altura=6;
+
+    for(int x = 1; x < 30; x+=2){
+        pVector.push_back(arco(pntx,pnty,ancho,altura, pi/2, 3*pi/2));
+        pntx+=6;
+        ancho++;
+        altura+=8;
+        
+    }
+    pVector.push_back(arco(pntx-4,pnty,0,altura-8,pi/2, 3*pi/2));
+    
+    altura-=8;
+    pVector.push_back(arco(pntx-2,pnty,ancho,altura,-(pi/2), + (pi/2)));
+    pVector.push_back(arco(pntx,pnty,ancho,altura,-(pi/2), +(pi/2)));
+    
+    for(int x = 1; x < 30; x+=2){
+        pVector.push_back(arco(pntx,pnty,ancho,altura,-(pi/2), +(pi/2)));
+        pntx+=9; //+=6 pero se pierden algunas lineas
+        ancho--;
+        altura-=8;
+        
+    }
+
+    // for (double x = 0; x <= 1; x = x + 0.01){
+    //     pVector.push_back(punto(x*pAnchura, y1*pAltura));
+    //     pVector.push_back(punto(x*pAnchura, y2*pAltura));
+    //     pVector.push_back(punto(x*pAnchura, y3*pAltura));
+    //     pVector.push_back(punto(x*pAnchura, y4*pAltura));
+
+    //     if (x < 0.25 || (x >= 0.5 && x < 0.75)){
+    //         y1 = y1 - 0.01;
+    //         y2 = y2 + 0.01;
+    //         y3 = y3 - 0.01;
+    //         y4 = y4 + 0.01;
+    //     } else{
+    //         y1 = y1 + 0.01;
+    //         y2 = y2 - 0.01;
+    //         y3 = y3 + 0.01;
+    //         y4 = y4 - 0.01;
+    //     }
+
+    //     if (y1 < 0.009){
+    //         y1 = 0;
+    //     }
+    // }
 }
 
