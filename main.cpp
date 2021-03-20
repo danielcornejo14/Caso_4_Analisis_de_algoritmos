c5b09fa9eed6274c3d69021644d80b1302bd354f
MauAguero-12 <mam.121002@gmail.com>
2021-03-19 18:59:39 -0600

    creacion de todos los cuadros

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 class arco{
 public:
-    int x;
-    int y;
+    double x;
+    double y;
     int ancho;
     int alto;
     double inicio;
     double final;
 
-    arco(int pX, int pY, int pAncho, int pAlto, double pInicio, double pFinal){
+    arco(double pX, double pY, int pAncho, int pAlto, double pInicio, double pFinal){
         x = pX;
         y = pY;
         ancho = pAncho;
@@ -23,12 +23,19 @@ public:
     }
 };
 
-void crearImagen(int pAltura, int pAnchura, vector<arco> &pVector);
+int crearImagen(int pAltura, int pAnchura, vector<arco> &pVector);
 
 int main(){
 
     vector<arco> vectorFigura;
-    crearImagen(5, 5, vectorFigura);
+    int size = crearImagen(500, 500, vectorFigura);
+
+    ofstream settings ("settings.txt");
+    if (settings.is_open())
+    {
+        settings << size;
+        settings.close();
+    }
 
     ofstream myfile ("caso4.txt");
     if (myfile.is_open())
@@ -41,15 +48,10 @@ int main(){
     }
     else cout << "Unable to open file";
 
-    // for(auto pun : vectorFigura){
-    //     cout <<  pun.x << ";" << pun.y << ";" << pun.ancho << ";" << pun.alto << ";"
-    //     << pun.inicio << ";" << pun.final << endl;
-    // }
-
     return 0;
 }
 
-void crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
+int crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
     double pi = 3.1415927;
 
     int size;
@@ -59,30 +61,57 @@ void crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
         size = pAnchura;
     }
 
-    int pntx=10;
-    int pnty=80;
-    
-    for(int i = 0; i < 3; i++){
-        int yHorizontal=24;
-        int anchoX=1;
-        int alturaX=6;
-        int anchoY=1;
-        int alturaY=6;
 
+    double pntx=-57.5;
+    double pnty=00;
+    double cont = -57.5;
+    int contLineas = 0;
+
+    int currentYHorizontal=-57.5;
     
+
+    int cantTotal = (size/115+2)* ((size/115+1)*2-1);
+    
+    for(int i = 0; i < cantTotal; i++){
+
+        if (cont > size){
+            contLineas++;
+            if (contLineas%2 == 1){
+                cont = 0;
+                pntx = 0;
+            } else{
+                cont = -57.5;
+                pntx = -57.5;
+            }
+            pnty += 57.5;
+            currentYHorizontal += 57.5;
+        }
+
+        int yHorizontal=currentYHorizontal;
+
+        int offsetX = 4;
+
+        int anchoX=1;
+        int alturaX=8;
+        int anchoY=1;
+        int alturaY=17;
+        
+
+        //verticales izq
         for(int x = 1; x < 30; x+=2){
             pVector.push_back(arco(pntx,pnty,anchoX,alturaX,pi/2, 3*pi/2));
-            pntx+=4;
+            pntx+=offsetX;
             anchoX++;
             alturaX+=8;
         }
         
-        pVector.push_back(arco(pntx-4,pnty,0,alturaX-8,pi/2, 3*pi/2));
         alturaX-=8;
-        pntx-=4;
+        pntx-=offsetX;
+        pVector.push_back(arco(pntx,pnty,0,alturaX,pi/2, 3*pi/2));
         
         
         
+        //horizontales arriba
         for(int x =1; x < 30; x+=2){
             pVector.push_back(arco(pntx,yHorizontal,anchoY,alturaY,-pi,0));
             yHorizontal+=4;
@@ -92,10 +121,12 @@ void crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
         
         }
         
-        pVector.push_back(arco(pntx,yHorizontal-4,anchoY-8,0,-pi,0));
         yHorizontal-=4;
         anchoY-=8;
+        pVector.push_back(arco(pntx,yHorizontal,anchoY,0,-pi,0));
         
+        
+        //horizontales abajo
         for(int x =1; x < 30; x+=2){
             pVector.push_back(arco(pntx,yHorizontal,anchoY,alturaY,0,pi));
             yHorizontal+=4;
@@ -103,38 +134,17 @@ void crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
             alturaY--;
         }
         
+        //verticales der
         for(int x = 1; x < 30; x+=2){
             pVector.push_back(arco(pntx,pnty,anchoX,alturaX,-(pi/2), +(pi/2)));
-            pntx+=4; //+=6 pero se pierden algunas lineas
+            pntx+=offsetX;
             anchoX--;
             alturaX-=8;
         }
+
+
+        cont += 115;
     }
-
-    // int pntx=10;
-    // int pnty=80;
-    // int ancho=1;
-    // int altura=6;
-
-    // for(int x = 1; x < 30; x+=2){
-    //     pVector.push_back(arco(pntx,pnty,ancho,altura, pi/2, 3*pi/2));
-    //     pntx+=6;
-    //     ancho++;
-    //     altura+=8;
-        
-    // }
-    // pVector.push_back(arco(pntx-4,pnty,0,altura-8,pi/2, 3*pi/2));
-    
-    // altura-=8;
-    // pVector.push_back(arco(pntx-2,pnty,ancho,altura,-(pi/2), + (pi/2)));
-    // pVector.push_back(arco(pntx,pnty,ancho,altura,-(pi/2), +(pi/2)));
-    
-    // for(int x = 1; x < 30; x+=2){
-    //     pVector.push_back(arco(pntx,pnty,ancho,altura,-(pi/2), +(pi/2)));
-    //     pntx+=9; //+=6 pero se pierden algunas lineas
-    //     ancho--;
-    //     altura-=8;
-        
-    // }
+    return size;
 }
 
