e33214be3ac10acf2be21451d774a9444e69d8b7
MauAguero-12 <mam.121002@gmail.com>
2021-03-19 19:39:40 -0600

    comentarios & tamano dinamico

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//Se define la clase arco que es usada para representar una linea curva
 class arco{
 public:
     double x;
@@ -29,16 +30,21 @@ int crearImagen(int pAltura, int pAnchura, vector<arco> &pVector);
 
 int main(){
 
+    //Se define un vector para almacenar todos los arcos y se llama a la funcion
     vector<arco> vectorFigura;
-    int size = crearImagen(1000, 1000, vectorFigura);
+    int size = crearImagen(230, 230, vectorFigura);
 
+    //
     ofstream settings ("settings.txt");
     if (settings.is_open())
     {
-        settings << size;
+        settings <<  size;
         settings.close();
     }
+    else cout << "Unable to open file";
 
+    //Se crea un archivo con todos los arcos que se crearon
+    //para poder abrir el archivo en processing 
     ofstream myfile ("caso4.txt");
     if (myfile.is_open())
     {
@@ -58,6 +64,7 @@ int main(){
 int crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
     double pi = 3.1415927;
 
+    //Se define el tamaño de la imagen (se hace cuadrada)
     int size;
     if (pAltura < pAnchura){
         size = pAltura;
@@ -66,6 +73,7 @@ int crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
     }
 
 
+    //Se definen los puntos para crear las lineas
     double pntx=-57.5;
     double pnty=00;
     double cont = -57.5;
@@ -75,11 +83,11 @@ int crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
     
     int largo = ((size - 57.5)/115) + 2;
 
+    //Se averigua el total de cuadrados por pintar
     int cantTotal = largo*largo + (largo)*(largo);
-    cout << cantTotal << endl;
     
     for(int i = 0; i < cantTotal; i++){
-
+        //Cada vez que se termina una fila, se hacen los ajustes para comenzar la siguiente
         if (cont >= size){
             contLineas++;
             if (contLineas%2 == 1){
@@ -93,6 +101,7 @@ int crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
             currentYHorizontal += 57.5;
         }
 
+        //Se definen mas variables para crear las lineas
         int yHorizontal=currentYHorizontal;
 
         int offsetX = 4;
@@ -103,7 +112,7 @@ int crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
         int alturaY=17;
         
 
-        //verticales izq
+        //Se crean las lineas verticales de la izquierda del cuadrado
         for(int x = 1; x < 30; x+=2){
             pVector.push_back(arco(pntx,pnty,anchoX,alturaX,pi/2, 3*pi/2));
             pntx+=offsetX;
@@ -117,7 +126,7 @@ int crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
         
         
         
-        //horizontales arriba
+        //Se crean las lineas horizontales de arriba del cuadrado
         for(int x =1; x < 30; x+=2){
             pVector.push_back(arco(pntx,yHorizontal,anchoY,alturaY,-pi,0));
             yHorizontal+=4;
@@ -132,7 +141,7 @@ int crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
         pVector.push_back(arco(pntx,yHorizontal,anchoY,0,-pi,0));
         
         
-        //horizontales abajo
+        //Se crean las lineas horizontales de abajo del cuadrado
         for(int x =1; x < 30; x+=2){
             pVector.push_back(arco(pntx,yHorizontal,anchoY,alturaY,0,pi));
             yHorizontal+=4;
@@ -140,7 +149,7 @@ int crearImagen(int pAltura, int pAnchura, vector<arco> &pVector){
             alturaY--;
         }
         
-        //verticales der
+        //Se crean las lineas verticales de la derecha del cuadrado
         for(int x = 1; x < 30; x+=2){
             pVector.push_back(arco(pntx,pnty,anchoX,alturaX,-(pi/2), +(pi/2)));
             pntx+=offsetX;
--- test1/test1.pde
+++ test1/test1.pde
@@ -1,6 +1,13 @@
+void settings(){
+  String[] settings = loadStrings("../settings.txt");
+  String[] para = split(settings[0], ";");
+  int x = Math.round(Float.valueOf(para[0]));
+  
+  size(x, x);
+}
+
 void setup(){
   
-  size(500,500);
   background(255);
   noFill();
   smooth();
@@ -10,5 +17,6 @@ void setup(){
   for(int i = 0; i < file.length; i++){
     String[] param = split(file[i], ";");
     arc(Float.valueOf(param[0]),Float.valueOf(param[1]),Float.valueOf(param[2]),Float.valueOf(param[3]),Float.valueOf(param[4]),Float.valueOf(param[5]));
+    
   }
 }
